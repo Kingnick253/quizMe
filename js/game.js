5422a8d4d9c11fceb62b07b0c7c7d43ec2da6a57
Kingnick253 <nicholaswebb2016@gmail.com>
2022-08-31 11:12:27 -0700

    Finished pseudo code and being to work through html page

--- js/game.js
+++ js/game.js
@@ -1,11 +1,15 @@
 // Declare count down
+let timeInterval;
+const timeEl = document.querySelector('#timer');
 
 //DECLARE  questions number
-
+let questionPosition = 0;
 const questions = [
     {
         //question
+        question:[],
         //answers
+        answers: ["Answer A", "Answer B", "Answer C"],
         //correct answer 
     },
     {
@@ -23,23 +27,72 @@ const questions = [
         //answers
         //correct answer 
     },
-
+    
 ];
+//  Declare a `timerInterval
+const currentQuestion = questions[questionPosition];
+for(let i = 0; i < questions.length; i++);
 
 //DELCARE the 'timeel'
-const timeEl = document.querySelector('#timer');
 
 
-// function to start game
-function startGame();
+// function to startgame
+function startGame(){
+    
     
     //needs to hide the start screen
-    // display the first questions
+    
+    //st the quesiton position to 0
+    
+    // display the current quesiton
+    
+    //set the starting value of ` countdown`
+    
+    // Start the timer `Starttimer`
+    // IF `countdown` === 0 THEN `endGame()`
+    
+}
 
-    //start timer
+// Function `endGame`
+    function endGame(){
+        //Hide the questions area
+
+        // show the recorded highscore
+
+        //CLEAR the timer interval to stop from running
 
 
+
+    }
 //function to answerQuestion
+    function answerQuestion(){
 
     //Check if the answer is right
     
+    // IF the answer is wrong
+    
+    // THEN we need to subtract from 'countdown'
+    
+    
+    // iNCREASING THE QUESITON POSITION BY 1   
+    // IF ive passed the last quesiton
+        //THEN `endgame`
+        
+    // ELSE 
+        // Display the next question
+
+    
+    // questionPosition++
+    // displayCurrentQuestion()
+}
+
+// Function `displayCurrentQuestion`
+    function displayCurrentQuestion(){
+        questions[questionPosition]
+        
+    }
+// Function `recordTheHighscore`
+    function recordHighscore(){
+
+        
+    }
