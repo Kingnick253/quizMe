913cca72109ae618d2d2abf549ef2555a980dd92
Kingnick253 <nicholaswebb2016@gmail.com>
2022-08-31 10:40:50 -0700

    Added more framework

--- js/game.js
+++ js/game.js
@@ -1,1 +1,45 @@
-// 
+// Declare count down
+
+//DECLARE  questions number
+
+const questions = [
+    {
+        //question
+        //answers
+        //correct answer 
+    },
+    {
+        //question
+        //answers
+        //correct answer 
+    },
+    {
+        //question
+        //answers
+        //correct answer 
+    },
+    {
+        //question
+        //answers
+        //correct answer 
+    },
+
+];
+
+//DELCARE the 'timeel'
+const timeEl = document.querySelector('#timer');
+
+
+// function to start game
+function startGame();
+    
+    //needs to hide the start screen
+    // display the first questions
+
+    //start timer
+
+
+//function to answerQuestion
+
+    //Check if the answer is right
+    
